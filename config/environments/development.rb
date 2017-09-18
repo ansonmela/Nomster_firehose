52fd5d73bcab63aebd588a6de0f02f55e9acbce5
Anson Melakayil <anson.melakayil@yahoo.com>
2017-09-18 21:34:05 +0000

    fixed server error issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,8 @@ Rails.application.configure do
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => 'public, max-age=172800'
-
-  elsif
+}
+  else
     config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
