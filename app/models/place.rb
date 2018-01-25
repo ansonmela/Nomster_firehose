cedd5cd2f4a0db30230b6695449a53d6e19a075f
Anson Melakayil <anson.melakayil@yahoo.com>
2018-01-25 05:04:49 +0000

    resolved security vulnerability

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ApplicationRecord
 	belongs_to :user
-	has_many :comments
-        has_many :photos
+	has_many :comments, dependent: :destroy
+    has_many :photos
 	validates :name, presence: true
 	validates :name, length: { minimum: 3 }
 	validates :address, presence: true
